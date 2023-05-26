7cbd5db0a29ca1da2b4665a3e38affd8a6fd8146
barroscamila <camilagdbarros@gmail.com>
2023-05-26 15:47:21 -0300

    react module finished

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,17 +1,15 @@
-import MinhaLista from "./componentes/MinhaLista";
+import Contador2 from "./componentes/Contador2";
 
-
-export default function App() {  
+export default function App() { 
   
-
   return (
-    <MinhaLista />
+    <Contador2 />
   )
 }
 
 
 /* 
-  RASCUNHO...
+  >> ANOTAÇÕES...
 
   - O React é composto por um ou mais componentes 
   - Um arquivo para cada componente (padrão)
@@ -26,5 +24,12 @@ export default function App() {
   - Listas e chaves: Todos os itens da lista precisam ter uma key="..."
 
   - Efeitos colaterais: alterar estado de elementos e criar efeitos, por exemplo
+
+  - Buscar dados: fetch('url') 
+  Pode-se usar .then() ou await
+
+  - Aplicar estilos: arquivo.css ou arquivo.module.css
+
+  - 
 
 */
---
+++ meu-projeto-react/src/componentes/Contador2.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import styles from '../button.module.css'
+
+export default function Contador2() {
+
+    const [contador, setContador] = useState(0)
+
+    function aumentar() {
+        setContador(contador + 1)
+    }
+
+    return (
+        <div className="container">
+            <h1>Meu Contador</h1>
+            <h2>{contador}</h2>
+            <button className={styles.myButton} onClick={aumentar}>aumentar!</button>
+        </div>
+    )
+}
+
